2c390652d7561d89c591485f9987932846176348
FoxieWithGoldenEyes <bartek57810@gmail.com>
2023-11-07 21:57:55 +0100

    Add GetColor() function definition

--- 3DEngine/3DEngine/GraphicsRenderer.cpp
+++ 3DEngine/3DEngine/GraphicsRenderer.cpp
@@ -36,3 +36,16 @@ void drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3, sf
 	drawLine(x2, y2, x3, y3, color);
 	drawLine(x3, y3, x1, y1, color);
 }
+
+/// <summary>
+/// Retutns a color between white and black depends on luminescence value
+/// </summary>
+/// <param name="lum"> A value between 0 and 1</param>
+/// <returns></returns>
+sf::Color GetColor(float lum)
+{
+    constexpr sf::Uint8 maxValue = std::numeric_limits<sf::Uint8>::max();
+    sf::Uint8 colorIntensity = static_cast<int>(maxValue * lum);
+    sf::Color color(lum * colorIntensity, lum * colorIntensity, lum * colorIntensity);
+	return color;
+}
